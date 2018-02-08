0447c5a508f5edff7363292d68cbc78ee59e52ad
NJUshawnli <lixu_shawn@foxmail.com>
2018-02-08 17:39:55 +0800

    some changes regarding plotting have been added

--- statistics_report/BasicStatistics.py
+++ statistics_report/BasicStatistics.py
@@ -10,13 +10,17 @@ import seaborn as sns
 
 class BasicStatisticsReport(AbstractStatistics):
 
-    def __init__(self, portfolio_handler):
+    def __init__(self, portfolio_handler, benchmark=None):
 
+        self.benchmark = None
         self.drawdowns = [0]
+        self.drawdowns_pct = [0.0]
         self.equity = []
+        self.equity_benchmark = [1.0]
         self.equity_returns = [0.0]
-
+        self.net_equity = [1.0]
         self.timeseries = ["0000-00-00 00:00:00"]
+
 
         current_equity = portfolio_handler.portfolio.equity
         self.high_water_mark = [current_equity]
@@ -26,16 +30,19 @@ class BasicStatisticsReport(AbstractStatistics):
 
         if timestamp != self.timeseries[-1]:
 
+
             current_equity = portfolio_handler.portfolio.equity
 
             self.equity.append(current_equity)
             self.timeseries.append(timestamp)
+            self.net_equity.append(self.equity[-1]/self.equity[0])
 
             pct = ((self.equity[-1] - self.equity[-2])/self.equity[-1])*100
             self.equity_returns.append(round(pct, 4))
 
             self.high_water_mark.append(max(self.high_water_mark[-1], self.equity[-1]))
             self.drawdowns.append(self.high_water_mark[-1]-self.equity[-1])
+            self.drawdowns_pct.append(-self.drawdowns[-1]/self.high_water_mark[-1])
 
 
     def get_results(self):
@@ -81,29 +88,41 @@ class BasicStatisticsReport(AbstractStatistics):
 
     def plot_results(self):
 
-        sns.set()
+        sns.set_style("whitegrid")
         fig = plt.figure()
-
+        current_palette = sns.color_palette()
 
         df = pd.DataFrame()
         df["equity"] = pd.Series(self.equity, index=self.timeseries)
-        df["equity_returns"] = pd.Series(self.equity_returns, index=self.timeseries)
+        df["net_equity"] = pd.Series(self.net_equity, index=self.timeseries)
         df["drawdowns"] = pd.Series(self.drawdowns, index=self.timeseries)
-        max_drawdown_pict = self.calculate_max_drawdown_pct()
-        df["max_drawdown_pct"] = pd.Series(max_drawdown_pict, index=self.timeseries)
+        df["drawdowns_pct"] = pd.Series(self.drawdowns_pct, index=self.timeseries)
 
-        ax1 = fig.add_subplot(221, ylabel='Equity')
-        df["equity"].plot(ax=ax1, color=sns.color_palette()[0])
+        ax1 = fig.add_subplot(221, ylabel='Equity Curve')
+        df["equity"].plot(ax=ax1, color=current_palette[0], label='Equity')
+        ax1.yaxis.grid(linestyle=':')
+        ax1.xaxis.grid(linestyle=':')
+        ax1.legend()
 
-        ax2 = fig.add_subplot(222, ylabel='Equity Returns')
-        df["equity_returns"].plot(ax=ax2, color=sns.color_palette()[1])
+        ax2 = fig.add_subplot(222, ylabel='Net Equity')
+        df["net_equity"].plot(ax=ax2, color=current_palette[1], label='Net Equity')
+        ax2.yaxis.grid(linestyle=':')
+        ax2.xaxis.grid(linestyle=':')
+        ax2.axhline(y=1, color=current_palette[1], linestyle='dashed')
+        ax2.legend()
 
         ax3 = fig.add_subplot(223, ylabel='Drawdowns')
-        df["drawdowns"].plot(ax=ax3, color=sns.color_palette()[2])
+        df["drawdowns"].plot(ax=ax3, color=current_palette[3], label='Drawdowns')
+        ax3.yaxis.grid(linestyle=':')
+        ax3.xaxis.grid(linestyle=':')
+        ax3.legend()
 
-        ax4 = fig.add_subplot(224, ylabel="max_drawdown_pct")
-        df["max_drawdown_pct"].plot(ax=ax4, color=sns.color_palette()[3])
-
+        ax4 = fig.add_subplot(224, ylabel="Drawdown pct")
+        ax4.yaxis.grid(linestyle=':')
+        ax4.xaxis.grid(linestyle=':')
+        df["drawdowns_pct"].plot(ax=ax4, kind='area', alpha=0.3,
+                                 color=current_palette[2], label='Drawndown Pct')
+        ax4.legend()
         plt.show()
 
     def calculate_active_sharp(self):
